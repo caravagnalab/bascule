01f3783fbf73c20a3e9240a80c126f352ac073bb
azadsadr <azad.sadr.h@gmail.com>
2022-05-03 12:26:25 +0200

    add basilica class constructor

--- basilica/R/signatureFinder.R
+++ basilica/R/signatureFinder.R
@@ -1,5 +1,18 @@
 
 
+# constructor
+basilica <- function(x) {
+  stopifnot(is.list(x))
+
+  res <- list(
+    exposure = x[[1]],
+    catalog_signatures = x[[2]],
+    denovo_signatures = x[[3]])
+
+  structure(res, class="basilica")
+}
+
+
 
 #' Title
 #'
@@ -22,30 +35,107 @@ fit <- function(x, input_catalog=NULL, k=0:5, reference_catalog=basilica::COSMIC
   x <- r_to_py(x)
   input_catalog <- r_to_py(input_catalog)
   #----------------------------- MUST BE CHANGED -------------------------------
-  setwd("/home/azad/Documents/thesis/SigPhylo/PyBaSiLiCa")
+  #import("pybasilica")
+  setwd("/home/azad/Documents/thesis/SigPhylo/pybasilica/src")
   source_python("basilica.py")
   py_run_string("k = list(map(int, [0, 1, 2, 3, 4, 5]))")
   k <- py$k
   k <- r_to_py(k)
+
+  py_run_string("steps_per_iter = 500")
+  steps_per_iter <- py$steps_per_iter
+  steps_per_iter <- r_to_py(steps_per_iter)
   #-----------------------------------------------------------------------------
   reference_catalog <- r_to_py(reference_catalog)
   lr <- r_to_py(lr)
-  steps_per_iter <- r_to_py(steps_per_iter)
+
   fixedLimit <- r_to_py(fixedLimit)
   denovoLimit <- r_to_py(denovoLimit)
 
   # add groups as one of the arguments later
   # groups: vector of discrete labels with one entry per sample, it defines the groups that will be considered by basilica
   #output <- BaSiLiCa(x, groups, input_catalog, k, reference_catalog, fixedLimit, denovoLimit)
-  output <- BaSiLiCa(x, input_catalog, k, reference_catalog, lr, steps_per_iter, fixedLimit, denovoLimit)
+  output <- pyfit(x, input_catalog, k, reference_catalog, lr, steps_per_iter, fixedLimit, denovoLimit)
+  print(paste("output class:", class(output)))
 
+  '
 
-  alpha <- output[[1]]
-  beta_fixed <- output[[2]]
-  beta_denovo <- output[[3]]
+  res <- list(exposure = output[[1]],
+              catalog_signatures = output[[2]],
+              denovo_signatures = output[[3]])
 
-  return(list(Alpha = alpha, Beta_Fixed = beta_fixed, Beta_Denovo = beta_denovo))
+  class(res) = "fit"
+  return(res)
+  '
+  return(output)
 }
+
+
+#-------------------------------------------------------------------------------
+
+
+library(reticulate)
+library(ggplot)
+'
+#-------------------------------------------------------------------------------
+pybasilica <- import("pybasilica")
+output <- PyBaSiLiCa$BaSiLiCa(M, B_input, k_list, cosmic_df, fixedLimit, denovoLimit)
+#-------------------------------------------------------------------------------
+'
+
+#' Title
+#'
+#' @param catalogue_path
+#'
+#' @return
+#' @export
+#'
+#' @examples
+readCatalogue <- function(catalogue_path) {
+
+  M <- read.table(catalogue_path,
+                  sep = ",",
+                  header = TRUE,
+                  stringsAsFactors = TRUE,
+                  check.names=FALSE)
+  return(M)
+}
+
+
+#' Title
+#'
+#' @param beta_path
+#'
+#' @return
+#' @export
+#'
+#' @examples
+readBeta <- function(beta_path) {
+
+  beta <- read.table(beta_path,
+                     sep = ",",
+                     header = TRUE,
+                     stringsAsFactors = TRUE,
+                     check.names=FALSE,
+                     row.names = 1)
+  return(beta)
+}
+
+
+#' Title
+#'
+#' @param x input mutational counts data (data.frame; rows as samples and columns as 96 mutational categories)
+#' @param groups vector of discrete labels with one entry per sample, it defines the groups that will be considered by basilica
+#' @param input_catalog input signature profiles, NULL by default
+#' @param k vector of possible number of de novo signatures to infer
+#' @param reference_catalog a catalog of reference signatures that basilica will use to compare input and de novo signatures
+#' @param fixedLimit threshold to discard the signature based on its value in exposure matrix
+#' @param denovoLimit threshold to consider inferred signature as COSMIC signature
+#'
+#' @return inferred exposure matrix, inferred COSMIC signatures and inferred non-COSMIC signatures
+#' @export
+#'
+#' @examples
 
 
 
