3cb40b3527aabd136063f345b48acd13176212ed
Giulio Caravagna <gcaravagn@gmail.com>
2022-09-27 12:50:53 +0200

    @azadsadr

---
+++ R/basilica-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom cli cli_abort
+#' @importFrom cli cli_alert_warning
+#' @importFrom cli cli_h1
+## usethis namespace: end
+NULL
