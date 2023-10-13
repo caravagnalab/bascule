14e18780a3f28011873998a72da5e11f3004bd16
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-10-13 14:49:39 +0200

    fit and getters functions

--- R/fit.R
+++ R/fit.R
@@ -1,118 +1,128 @@
 # counts is a list with input matrices, names are typenames
-fit = function(counts) {
+fit = function(counts, k_list, cluster, reference_cat,
+               keep_sigs = c("SBS1","SBS5"),
+
+               hyperparameters = NULL,
+
+               lr = 0.005,
+               optim_gamma = 0.1,
+               n_steps = 3000,
+               py = NULL,
+
+               stage = "",  # remove
+
+               enumer = "parallel",
+               nonparametric = TRUE,
+
+               dirichlet_prior = TRUE,  # remove
+               enforce_sparsity = TRUE,  # remove
+
+               filter_dn = TRUE,
+               min_exposure = 0.2,
+               CUDA = TRUE,
+               compile = FALSE,
+
+               store_parameters = FALSE,
+               save_all_fits = FALSE,
+
+               reg_weight = 0.,
+               regularizer = "cosine",
+               regul_compare = NULL,
+               regul_denovo = TRUE,
+               regul_fixed = TRUE,
+
+               seed_list = c(10)) {
   if (!is.list(counts)) counts = list("T1"=counts)
+  if (!is.list(reference_cat)) reference_cat = list("T1"=reference_cat)
 
   bas = list(); class(bas) = "basilica_obj"
 
+  types = names(counts)
+
   # input contains a list of counts matrices
-  bas$input = lapply(counts, wide_to_long, what="counts")
+  bas$input = lapply(types, function(t) {
+    list(counts=wide_to_long(counts[[t]], what="counts"),
+         reference=wide_to_long(reference_cat[[t]], what="beta"))
+  }) %>% setNames(types)
 
   # nmf contains the pyro fits
-  types = names(counts)
   bas$nmf = lapply(types, function(t)
-    pyro_nmf(counts=counts[[t]],
-             k_list=k_list,
-             lr=lr,
+    pyro_nmf(counts = counts[[t]],
+             k_list = k_list,
+             reference_cat = reference_cat[[t]],
+             keep_sigs = keep_sigs,
+
+             hyperparameters = hyperparameters,
+
+             lr = lr,
              optim_gamma = optim_gamma,
              n_steps = n_steps,
-             stage = stage,
-             py = py,
-             dirichlet_prior = dirichlet_prior,
-             beta_fixed = beta_fixed,
-             keep_sigs = keep_sigs,
+
+             stage = stage,  # remove
+
+             dirichlet_prior = dirichlet_prior,  # remove
+             enforce_sparsity = enforce_sparsity,  # remove
+
+             filter_dn = filter_dn,
              min_exposure = min_exposure,
-             hyperparameters = hyperparameters,
              CUDA = CUDA,
              compile = compile,
-             enforce_sparsity = enforce_sparsity,
+
              store_parameters = store_parameters,
+             save_all_fits = save_all_fits,
+
+             reg_weight = reg_weight,
              regularizer = regularizer,
              regul_compare = regul_compare,
-             reg_weight = reg_weight,
-             seed_list = seed_list,
              regul_denovo = regul_denovo,
              regul_fixed = regul_fixed,
-             save_all_fits = save_all_fits,
-             filter_dn = filter_dn)
+
+             seed_list = seed_list,
+             py = py)
   ) %>% setNames(types)
 
   # clustering contains the clustering
-  expos = get_exposure(bas, matrix=TRUE)
-  bas$clustering = pyro_clustering(exposures=expos, type=t, ...)
+  exposures = get_exposure(bas, matrix=TRUE)
+  bas$clustering = pyro_clustering(exposures = exposures,
+                                   cluster = cluster,
 
+                                   enumer = enumer,
+                                   nonparametric = nonparametric,
+
+                                   hyperparameters = hyperparameters,
+
+                                   lr = lr,
+                                   optim_gamma = optim_gamma,
+                                   n_steps = n_steps,
+
+                                   CUDA = CUDA,
+
+                                   store_parameters = store_parameters,
+                                   save_all_fits = save_all_fits,
+
+                                   seed_list = seed_list,
+                                   py = py)
   return(bas)
 }
 
 
-# add args for pyro fit; returns nmf for type t
-pyro_nmf = function(..., k_list, beta_fixed, stage, cohort,
-                    filter_dn, min_exposure, keep_sigs) {
+gen_palette = function(x=NULL, types=get_types(x), n=NULL) {
+  if (!is.null(n)) return(ggsci::pal_simpsons()(n))
 
-  pyro_fit = nmf_single_type(..., k_list=k_list, beta_fixed=beta_fixed,
-                             stage=stage, cohort=cohort, filter_dn=filter_dn,
-                             min_exposure=min_exposure, keep_sigs=keep_sigs)
+  denovo_names = unique(unlist(get_denovo_signames(x, types=types)))
 
-  nmf_t = list(pyro=pyro_fit, exposure=pyro_fit$exposure,
-               beta_fixed=pyro_fit$beta_fixed, beta_denovo=pyro_fit$beta_denovo)
-  # x$nmf[[type]] = nmf_t
-  return(nmf_t)
-}
-
-
-nmf_single_type = function(..., k_list, beta_fixed, stage, cohort,
-                           filter_dn, min_exposure, keep_sigs) {
-  TIME = as.POSIXct(Sys.time(), format = "%H:%M:%S")
-  call_info = match.call()
-
-  if (!is.null(beta_fixed)) {
-    x_ref = pyfit(..., k_list=0, beta_fixed=beta_fixed,
-                  stage="random_noise", filter_dn=FALSE)
-
-    x_ref_filt = x_ref %>% filter_sigs_low_expos(min_exp=min_exposure, keep_sigs=keep_sigs)
-    catalogue2 = long_to_wide(dataframe=x_ref_filt$beta_fixed, what="beta")
-  } else {
-    x_ref = x_ref_filt = catalogue2 = NULL
-    residues = FALSE
-  }
-
-  x_dn = pyfit(..., k_list=k_list, beta_fixed=catalogue2, stage=stage, filter_dn=filter_dn)
-
-  TIME = difftime(as.POSIXct(Sys.time(), format = "%H:%M:%S"), TIME, units = "mins")
-
-  x_dn$time = TIME
-  x_dn$k_list = k_list
-  x_dn$call = call_info
-
-  return(x_dn)
-}
-
-
-renormalize_denovo_thr = function(py_obj, thr=0.02, filter_dn=TRUE) {
-  if (is.null(py_obj$beta_denovo)) return(NULL)
-
-  py_obj$beta_denovo = py_obj$beta_denovo %>%
-      dplyr::mutate(value=replace(value, value<thr, 0)) %>%
-      dplyr::group_by(sigs) %>%
-      dplyr::mutate(value=value/sum(value)) %>%
-      dplyr::ungroup()
-  return(py_obj)
-}
-
-
-gen_palette = function(x, type) {
-  ref = COSMIC_color_palette(catalogue=get_fixed_signatures(x, type=type, wide=T)) %>%
-    setNames(get_fixed_signames(x, type=type))
-  dn = ggsci::pal_simpsons()(length(get_denovo_signames(x))) %>%
-    setNames(get_denovo_signames(x))
+  ref = COSMIC_color_palette(signames=get_fixed_signames(x, types=types))
+  dn = ggsci::pal_simpsons()(length(denovo_names)) %>%
+    setNames(denovo_names)
   return(c(ref, dn))
 }
 
 
-COSMIC_color_palette = function(catalogue=COSMIC_filt, seed=14) {
-  N = nrow(catalogue)
+COSMIC_color_palette = function(signames=get_signames(x), seed=14) {
+  N = length(unique(unlist(signames)))
   set.seed(seed)
   colss = Polychrome::createPalette(N, c("#856de3","#9e461c"), target="normal", range=c(15,80), M=1000)[1:N]
-  names(colss) = rownames(catalogue)
+  names(colss) = unique(unlist(signames))
   return(colss)
 }
 
--- R/getters.R
+++ R/getters.R
@@ -1,17 +1,78 @@
-get_fixed_signames = function(x, type="T1") {
-  return(get_fixed_signatures(x, type=type)$sigs %>% unique())
+get_K = function(x, types=get_types(x)) {
+  return(lapply(get_signames(x, types=types), length))
 }
 
 
-get_denovo_signames = function(x, type="T1") {
-  return(get_denovo_signatures(x, type=type)$sigs %>% unique())
+get_G = function(x, input=FALSE) {
+  if (!input) return(length(get_cluster_labels(x)))
+  return(
+    get_pyro_stat(x, what="clustering",
+                  statname="params")[["infered_params"]]$post_probs %>% ncol()
+  )
+}
+
+
+get_seed = function(x, what, types=get_types(x)) {
+  return(get_pyro_stat(x, what=what, statname="seed"))
+}
+
+
+get_fixed_signames = function(x, types=get_types(x)) {
+  lapply(types, function(t)
+    get_fixed_signatures(x, types=t, matrix=TRUE)[[t]] %>% rownames()) %>%
+    setNames(types)
+}
+
+get_denovo_signames = function(x, types=get_types(x)) {
+  lapply(types, function(t)
+    get_denovo_signatures(x, types=t, matrix=TRUE)[[t]] %>% rownames()) %>%
+    setNames(types)
+}
+
+get_signames = function(x, types=get_types(x)) {
+  lapply(types, function(t)
+    get_signatures(x, types=t, matrix=TRUE)[[t]] %>% rownames()) %>%
+    setNames(types)
+}
+
+
+get_fixed_signatures = function(x, types=get_types(x), samples=get_samples(x),
+                                clusters=get_cluster_labels(x), matrix=FALSE) {
+  return(get_signatures_aux(x=x, types=types, matrix=matrix, what="fixed"))
+}
+
+
+get_denovo_signatures = function(x, types=get_types(x), samples=get_samples(x),
+                                 clusters=get_cluster_labels(x), matrix=FALSE) {
+  return(get_signatures_aux(x=x, types=types, matrix=matrix, what="denovo"))
+}
+
+
+# Get signatures, it can subset by types and return a list of matrices.
+get_signatures = function(x, types=get_types(x), matrix=FALSE) {
+  return(get_signatures_aux(x=x, types=types, matrix=matrix, what="all"))
+}
+
+
+## what %in% c("denovo","fixed","all")
+get_signatures_aux = function(x, what, types=get_types(x), matrix=FALSE) {
+  out = lapply(types, function(t) {
+    if (what=="fixed") x$nmf[[t]]$beta_fixed else if (what=="denovo")
+      x$nmf[[t]]$beta_denovo else if (what=="all")
+        rbind(x$nmf[[t]]$beta_fixed, x$nmf[[t]]$beta_denovo) else NULL
+  }) %>% setNames(types)
+
+  if(matrix)
+    out = lapply(out, function(df_t) long_to_wide(df_t, what="beta")) %>%
+      setNames(types)
+  return(out)
 }
 
 
 get_input = function(x, types=get_types(x), samples=get_samples(x),
                      clusters=get_cluster_labels(x), matrix=FALSE) {
   out = lapply(types, function(t) {
-    w = x$input[[t]] %>%
+    w = x$input[[t]]$counts %>%
       dplyr::filter(samples %in% !!samples)
 
     if(!is.null(clusters)) {
@@ -19,7 +80,7 @@ get_input = function(x, types=get_types(x), samples=get_samples(x),
         get_cluster_assignments(samples=samples, clusters=clusters) %>%
         dplyr::pull(samples)
 
-      w = w %>% filter(samples %in% which_selection)
+      w = w %>% dplyr::filter(samples %in% which_selection)
     }
 
     return(w)
@@ -34,17 +95,18 @@ get_input = function(x, types=get_types(x), samples=get_samples(x),
 # Get exposure, it can subset by types, samples and clusters. It can return
 # a list of matrices.
 get_exposure = function(x, types=get_types(x), samples=get_samples(x),
-                        clusters=get_cluster_labels(x), matrix=FALSE) {
+                        clusters=get_cluster_labels(x), add_groups=FALSE,
+                        matrix=FALSE) {
   out = lapply(types, function(t) {
       w = x$nmf[[t]]$exposure %>%
         dplyr::filter(samples %in% !!samples)
 
       if(!is.null(clusters)) {
         which_selection = x %>%
-          get_cluster_assignments(samples=samples, clusters=clusters) %>%
-          dplyr::pull(samples)
+          get_cluster_assignments(samples=samples, clusters=clusters)
 
-        w = w %>% filter(samples %in% which_selection)
+        w = w %>% dplyr::inner_join(which_selection, by="samples")
+        if (!add_groups) w = w %>% dplyr::select(-clusters)
       }
       return(w)
     }) %>% setNames(types)
@@ -55,56 +117,42 @@ get_exposure = function(x, types=get_types(x), samples=get_samples(x),
   return(out)
 }
 
-# Get signatures, it can subset by types and return
-# a list of matrices.
-get_signatures = function(x, types=get_types(x), matrix=FALSE) {
-  out = lapply(types, function(t) {
-    w = x$nmf[[t]]$signatures %>%
-      dplyr::filter(samples %in% !!samples)
-
-    if(!is.null(clusters)) {
-      which_selection = x %>%
-        get_cluster_assignments(samples = samples, clusters = clusters) %>%
-        dplyr::pull(samples)
-      w = w %>% filter(samples %in% which_selection)
-    }
-    return(w)
-  }) %>% setNames(types)
-
-  if(matrix)
-    out = lapply(out, function(df_t) long_to_wide(df_t, what="beta")) %>%
-      setNames(types)
-  return(out)
-}
 
 
 # Get cluster labels ("C1", "C2")
 get_cluster_labels = function(x) {
   if(is.null(x$clustering)) return(NULL)
 
-  x$clustering$cluster %>% unique()
+  x$clustering$cluster$clusters %>% unique()
 }
 
 # Get clustering assignments (tibble)
 get_cluster_assignments = function(x, samples=get_samples(x), clusters=get_cluster_labels(x)) {
   if(is.null(x$clustering)) return(NULL)
 
-  x$clustering %>%
-    dplyr::filter(samples %in% samples,
-                  cluster %in% clusters)
+  return(
+    x$clustering$clusters %>%
+      dplyr::filter(samples %in% !!samples,
+                    clusters %in% !!clusters)
+  )
 }
 
 
 # Get type of data used for signatures: e.g., "SBS", "DBS"
-get_types = function(x) {x$input %>% names()}
+get_types = function(x) {
+  if (is.null(x)) return(NULL)
+  return(x$input %>% names())
+}
 
 
 # Get samples names
-get_samples = function(x) {x$input[[1]]$samples %>% unique()}
+get_samples = function(x) {x$input[[1]]$counts$samples %>% unique()}
 
 
 # what %in% c("beta","exposures","counts")
 wide_to_long = function(dataframe, what) {
+  if (is.null(dataframe) || nrow(dataframe)==0) return(NULL)
+
   cols = dplyr::case_when(
     what == "beta" ~ list(variables="features", ids="sigs"),
     what == "exposures" ~ list(variables="sigs", ids="samples"),
@@ -113,12 +161,13 @@ wide_to_long = function(dataframe, what) {
   dataframe %>% as.data.frame() %>%
     tibble::rownames_to_column(var=cols$ids) %>%
     reshape2::melt(id=cols$ids, variable.name=cols$variables) %>%
+    dplyr::mutate(dplyr::across(is.factor, as.character)) %>%
     tibble::as_tibble()
 }
 
 
 long_to_wide = function(dataframe, what) {
-  if (is.null(dataframe) | nrow(dataframe)==0) return(NULL)
+  if (is.null(dataframe) || nrow(dataframe)==0) return(NULL)
   cols = dplyr::case_when(
     what == "beta" ~ list(variables="features", ids="sigs"),
     what == "exposures" ~ list(variables="sigs", ids="samples"),
