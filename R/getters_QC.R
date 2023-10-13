83b36487b6fb5bccf23433a35411cb3f7675a3cb
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-10-13 14:51:09 +0200

    getters for QC stats

---
+++ R/getters_QC.R
@@ -0,0 +1,74 @@
+get_gradient_norms = function(x, types=get_types(x)) {
+  vname = "gradient_norms"
+  qcs_nmf = get_QC(x, what="nmf", types=types)
+  qcs_clustering = get_QC(x, what="clustering")
+  norms_nmf = lapply(types, function(tid) qcs_nmf[[tid]][[vname]] %>% as.data.frame() %>%
+           tibble::rownames_to_column(var="step") %>%
+           reshape2::melt(id="step", variable.name="parameter", value.name="value") %>%
+           dplyr::mutate(type=tid)) %>% do.call(rbind, .)
+  norms_clustering = qcs_clustering[[vname]] %>% as.data.frame() %>%
+    tibble::rownames_to_column(var="step") %>%
+    reshape2::melt(id="step", variable.name="parameter", value.name="value") %>%
+    dplyr::mutate(type="Clustering")
+
+  return(rbind(norms_nmf, norms_clustering))
+}
+
+
+get_scores = function(x, types=get_types(x)) {
+  vname = "scores"
+  qcs_nmf = get_QC(x, what="nmf", types=types)
+  qcs_clustering = get_QC(x, what="clustering")
+
+  scores_nmf = lapply(types, function(tid) qcs_nmf[[tid]][[vname]] %>% as.data.frame() %>%
+                        dplyr::mutate(type=tid)) %>%
+    do.call(rbind, .)
+  scores_clustering = qcs_clustering[[vname]] %>% dplyr::mutate(type="Clustering")
+
+  return(rbind(scores_nmf, scores_clustering) %>%
+           dplyr::mutate(dplyr::across(.cols=c("seed","value"), as.integer)) %>%
+           tibble::as_tibble())
+}
+
+
+# params = list("K"=NA,"G"=NA,"seed"=NA)
+get_alternative_run = function(x, params=list("K"=NA,"G"=NA,"seed"=NA), types=get_types(x)) {
+  G = params$G; K = params$K; seed = params$seed
+  if (all(is.na(params))) return(cli::cli_alert_warning("No input were provided."))
+
+  if (is.na(seed)) seed = list("nmf"=get_seed(x, what="nmf", types=types),
+                               "clustering"=get_seed(x, what="clustering"))
+  if (is.na(G)) G = get_G(x, input=TRUE)
+  if (is.na(K)) K = get_K(x)
+  k_vname = paste0("k_denovo:", K)
+  g_vname = paste0("cluster:", G)
+  x$clustering = get_alternatives(x, what="clustering")$runs_seed[[paste0("seed:", seed$clustering)]]
+  alter_nmf = get_alternatives(x, what="nmf", types=types)
+  x$nmf = lapply(types, function(tid) alter_nmf[[tid]]$runs_seed[[paste0("seed:", seed$nmf[[tid]])]]) %>%
+    setNames(types)
+
+}
+
+
+## what %in% c("nmf", "clustering")
+get_alternatives = function(x, what, types=get_types(x)) {
+  return(get_pyro_stat(x=x, what=what, statname="alternatives", types=types))
+}
+
+
+## what %in% c("nmf", "clustering")
+get_QC = function(x, what, types=get_types(x)) {
+  return(get_pyro_stat(x=x, what=what, statname="QC", types=types))
+}
+
+
+get_pyro_stat = function(x, what, statname, types=get_types(x)) {
+  what = tolower(what)
+  if (what=="nmf") return(
+    lapply(types, function(tid) x[[what]][[tid]][["pyro"]][[statname]]) %>% setNames(types)
+  )
+  if (what=="clustering") return(x[[what]][["pyro"]][[statname]])
+  cli::cli_alert_warning("`what` must be either 'nmf' or 'clustering'")
+}
+
+
--- R/giulio.R
+++ R/giulio.R
@@ -1,198 +1,198 @@
-# Sanitize input (check all patients are defined for every data type)
-sanitizer_long = function(x)
-{
-  shared_samples = lapply(
-    x,
-    function(x){
-      x$sample %>% unique()
-    }
-  ) %>%
-    Reduce(f = intersect)
-
-  test_in = sapply(
-    x, function(x) all(x$sample %in% shared_samples)
-  ) %>%
-    Reduce(f = all)
-
-  stopifnot(test_in)
-}
-
-# Internal, everything in long tibble format
+# # Sanitize input (check all patients are defined for every data type)
+# sanitizer_long = function(x)
+# {
+#   shared_samples = lapply(
+#     x,
+#     function(x){
+#       x$sample %>% unique()
+#     }
+#   ) %>%
+#     Reduce(f = intersect)
 #
-# - input: list with data (sample | type | feature | value)
-# - nmf: list with Pyro fit, exposures and signatures tibbles (sample | type | signature | value)
-# - clustering: list with a tibble  (sample | cluster)
-# - stat: named vector with number of patients, number of data types, number of signatures per data type,
-#         number of clusters, fit scores, etc.
-# - alternative: alternative model fits
-
-# Get type of data used for signatures: e.g., "SBS", "DBS"
-get_types = function(x) { x$input %>% names() }
-
-# Get samples names
-get_samples = function(x) { x$input[[1]]$sample %>% unique() }
-
-# Get cluster labels ("C1", "C2")
-get_cluster_labels = function(x)
-{
-  if(is.null(x$clustering)) return(NULL)
-
-  x$clustering$cluster %>% unique()
-}
-
-# Get clustering assignments (tibble)
-get_cluster_assignments = function(x,
-                                   samples = get_samples(x),
-                                   clusters = get_cluster_labels(x)
-)
-{
-  if(is.null(x$clustering)) return(NULL)
-
-  x$clustering %>%
-    dplyr::filter(
-      sample %in% samples,
-      cluster %in% clusters
-    )
-}
-
-# Get input, can subset by types, samples and clustering assignment (if available), and
-# can transform the data in matrix format
-get_input = function(x,
-                     types = get_types(x),
-                     samples = get_samples(x),
-                     clusters = get_cluster_labels(x),
-                     matrix = FALSE
-)
-{
-  out = lapply(
-    types,
-    function(t){
-      w = x$input[[t]] %>%
-        dplyr::filter(sample %in% !!samples)
-
-      if(!is.null(clusters))
-      {
-        which_selection = x %>%
-          get_cluster_assignments(samples = samples, clusters = clusters) %>%
-          dplyr::pull(sample)
-
-        w = w %>% filter(sample %in% which_selection)
-      }
-
-      return(w)
-    })
-
-  if(matrix)
-  {
-    out = lapply(out,
-                 function(x){
-                   x %>%
-                     dplyr::select(-type) %>%
-                     tidyr::pivot_wider(
-                       names_from = feature,
-                       values_from = value
-                     )
-                 }
-    )
-  }
-
-  return(out)
-}
-
-# Get exposure, it can subset by types, samples and clusters. It can return
-# a list of matrices.
-get_exposure = function(x,
-                        types = get_types(x),
-                        samples = get_samples(x),
-                        clusters = get_cluster_labels(x),
-                        matrix = FALSE
-)
-{
-  out = lapply(
-    types,
-    function(t){
-      w = x$nmf[[t]]$exposure %>%
-        dplyr::filter(sample %in% !!samples)
-
-      if(!is.null(clusters))
-      {
-        which_selection = x %>%
-          get_cluster_assignments(samples = samples, clusters = clusters) %>%
-          dplyr::pull(sample)
-
-        w = w %>% filter(sample %in% which_selection)
-      }
-
-      return(w)
-    })
-
-  if(matrix)
-  {
-    out = lapply(out,
-                 function(x){
-                   x %>%
-                     dplyr::select(-type) %>%
-                     tidyr::pivot_wider(
-                       names_from = feature,
-                       values_from = value
-                     )
-                 }
-    )
-  }
-}
-
-# Get signatures, it can subset by types and return
-# a list of matrices.
-get_signatures = function(
-    x,
-    types = get_types(x),
-    matrix = FALSE
-){
-  out = lapply(
-    types,
-    function(t){
-      w = x$nmf[[t]]$signatures %>%
-        dplyr::filter(sample %in% !!samples)
-
-      if(!is.null(clusters))
-      {
-        which_selection = x %>%
-          get_cluster_assignments(samples = samples, clusters = clusters) %>%
-          dplyr::pull(sample)
-
-        w = w %>% filter(sample %in% which_selection)
-      }
-
-      return(w)
-    })
-
-  if(matrix)
-  {
-    out = lapply(out,
-                 function(x){
-                   x %>%
-                     dplyr::select(-type) %>%
-                     tidyr::pivot_wider(
-                       names_from = feature,
-                       values_from = value
-                     )
-                 }
-    )
-  }
-}
-
-# NMF
-pyro_nmf = function(x, which){
-
-  fit = .... # pyro reticulate call
-
-  nmf = list(
-    pyro = fit,
-    exposure = ..., # extract in long format the exposure tibble
-    beta = ... # extract in long format the signatures tibble
-  )
-
-  x$nmf[[which]] = nmf # store the output
-
-  return(x)
-}
+#   test_in = sapply(
+#     x, function(x) all(x$sample %in% shared_samples)
+#   ) %>%
+#     Reduce(f = all)
+#
+#   stopifnot(test_in)
+# }
+#
+# # Internal, everything in long tibble format
+# #
+# # - input: list with data (sample | type | feature | value)
+# # - nmf: list with Pyro fit, exposures and signatures tibbles (sample | type | signature | value)
+# # - clustering: list with a tibble  (sample | cluster)
+# # - stat: named vector with number of patients, number of data types, number of signatures per data type,
+# #         number of clusters, fit scores, etc.
+# # - alternative: alternative model fits
+#
+# # Get type of data used for signatures: e.g., "SBS", "DBS"
+# get_types = function(x) { x$input %>% names() }
+#
+# # Get samples names
+# get_samples = function(x) { x$input[[1]]$sample %>% unique() }
+#
+# # Get cluster labels ("C1", "C2")
+# get_cluster_labels = function(x)
+# {
+#   if(is.null(x$clustering)) return(NULL)
+#
+#   x$clustering$cluster %>% unique()
+# }
+#
+# # Get clustering assignments (tibble)
+# get_cluster_assignments = function(x,
+#                                    samples = get_samples(x),
+#                                    clusters = get_cluster_labels(x)
+# )
+# {
+#   if(is.null(x$clustering)) return(NULL)
+#
+#   x$clustering %>%
+#     dplyr::filter(
+#       sample %in% samples,
+#       cluster %in% clusters
+#     )
+# }
+#
+# # Get input, can subset by types, samples and clustering assignment (if available), and
+# # can transform the data in matrix format
+# get_input = function(x,
+#                      types = get_types(x),
+#                      samples = get_samples(x),
+#                      clusters = get_cluster_labels(x),
+#                      matrix = FALSE
+# )
+# {
+#   out = lapply(
+#     types,
+#     function(t){
+#       w = x$input[[t]] %>%
+#         dplyr::filter(sample %in% !!samples)
+#
+#       if(!is.null(clusters))
+#       {
+#         which_selection = x %>%
+#           get_cluster_assignments(samples = samples, clusters = clusters) %>%
+#           dplyr::pull(sample)
+#
+#         w = w %>% filter(sample %in% which_selection)
+#       }
+#
+#       return(w)
+#     })
+#
+#   if(matrix)
+#   {
+#     out = lapply(out,
+#                  function(x){
+#                    x %>%
+#                      dplyr::select(-type) %>%
+#                      tidyr::pivot_wider(
+#                        names_from = feature,
+#                        values_from = value
+#                      )
+#                  }
+#     )
+#   }
+#
+#   return(out)
+# }
+#
+# # Get exposure, it can subset by types, samples and clusters. It can return
+# # a list of matrices.
+# get_exposure = function(x,
+#                         types = get_types(x),
+#                         samples = get_samples(x),
+#                         clusters = get_cluster_labels(x),
+#                         matrix = FALSE
+# )
+# {
+#   out = lapply(
+#     types,
+#     function(t){
+#       w = x$nmf[[t]]$exposure %>%
+#         dplyr::filter(sample %in% !!samples)
+#
+#       if(!is.null(clusters))
+#       {
+#         which_selection = x %>%
+#           get_cluster_assignments(samples = samples, clusters = clusters) %>%
+#           dplyr::pull(sample)
+#
+#         w = w %>% filter(sample %in% which_selection)
+#       }
+#
+#       return(w)
+#     })
+#
+#   if(matrix)
+#   {
+#     out = lapply(out,
+#                  function(x){
+#                    x %>%
+#                      dplyr::select(-type) %>%
+#                      tidyr::pivot_wider(
+#                        names_from = feature,
+#                        values_from = value
+#                      )
+#                  }
+#     )
+#   }
+# }
+#
+# # Get signatures, it can subset by types and return
+# # a list of matrices.
+# get_signatures = function(
+#     x,
+#     types = get_types(x),
+#     matrix = FALSE
+# ){
+#   out = lapply(
+#     types,
+#     function(t){
+#       w = x$nmf[[t]]$signatures %>%
+#         dplyr::filter(sample %in% !!samples)
+#
+#       if(!is.null(clusters))
+#       {
+#         which_selection = x %>%
+#           get_cluster_assignments(samples = samples, clusters = clusters) %>%
+#           dplyr::pull(sample)
+#
+#         w = w %>% filter(sample %in% which_selection)
+#       }
+#
+#       return(w)
+#     })
+#
+#   if(matrix)
+#   {
+#     out = lapply(out,
+#                  function(x){
+#                    x %>%
+#                      dplyr::select(-type) %>%
+#                      tidyr::pivot_wider(
+#                        names_from = feature,
+#                        values_from = value
+#                      )
+#                  }
+#     )
+#   }
+# }
+#
+# # NMF
+# pyro_nmf = function(x, which){
+#
+#   fit = .... # pyro reticulate call
+#
+#   nmf = list(
+#     pyro = fit,
+#     exposure = ..., # extract in long format the exposure tibble
+#     beta = ... # extract in long format the signatures tibble
+#   )
+#
+#   x$nmf[[which]] = nmf # store the output
+#
+#   return(x)
+# }
