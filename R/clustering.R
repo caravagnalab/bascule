e6d1e6964b740f694f7a067037a4cd2f64d70228
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-10-13 14:49:27 +0200

    adding functions to extract pyro outputs

---
+++ R/clustering.R
@@ -0,0 +1,43 @@
+pyro_clustering = function(exposures, cluster, lr=0.005, n_steps=3000,
+                           optim_gamma=0.1, enumer="parallel",
+                           hyperparameters=NULL, nonparametric=TRUE,
+                           store_parameters=FALSE, save_all_fits=FALSE,
+                           seed_list=c(10), CUDA=TRUE, py=NULL) {
+
+  if (is.null(cluster)) return(NULL)
+
+  TIME = as.POSIXct(Sys.time(), format = "%H:%M:%S")
+
+  if (is.null(py)) py = reticulate::import("pybasilica")
+  cluster = reticulate::r_to_py(list(as.integer(cluster)))
+  seed_list = reticulate::r_to_py(as.integer(seed_list))
+  input_expos = reticulate::r_to_py(exposures %>% setNames(NULL))
+
+  obj = py$fit(alpha=input_expos, cluster=cluster, n_steps=n_steps,
+               lr=lr, optim_gamma=optim_gamma, hyperparameters=hyperparameters,
+               enumer=enumer, nonparametric=nonparametric, seed=seed_list,
+               CUDA=CUDA, store_parameters=store_parameters, save_all_fits=save_all_fits)
+
+  TIME = difftime(as.POSIXct(Sys.time(), format = "%H:%M:%S"), TIME, units = "mins")
+
+  if (is.list(obj)) {
+    bestRun = obj[[1]]
+    secondBest = obj[[2]]
+  } else {
+    bestRun = obj
+    secondBest = NULL
+  }
+
+  # save python object data in a list
+  pyro_fit = get_list_from_py_clustering(bestRun)
+  pyro_fit$pyro$alternatives$secondBest = get_list_from_py(secondBest, filter_dn=filter_dn)
+  pyro_fit$pyro$time = TIME
+
+  # clustering = list(pyro=pyro_fit,
+  #                   clusters=pyro_fit$clusters,
+  #                   centroids=pyro_fit$centroids)
+
+  return(pyro_fit)
+
+}
+
---
+++ R/nmf.R
@@ -0,0 +1,58 @@
+# add args for pyro fit; returns nmf for type t
+pyro_nmf = function(..., k_list, reference_cat, stage, cohort,
+                    filter_dn, min_exposure, keep_sigs) {
+
+  pyro_fit = nmf_single_type(..., k_list=k_list, reference_cat=reference_cat,
+                             stage=stage, cohort=cohort, filter_dn=filter_dn,
+                             min_exposure=min_exposure, keep_sigs=keep_sigs)
+
+  nmf_t = list(pyro=pyro_fit, exposure=pyro_fit$exposure,
+               beta_fixed=pyro_fit$beta_fixed, beta_denovo=pyro_fit$beta_denovo)
+  # x$nmf[[type]] = nmf_t
+  return(nmf_t)
+}
+
+
+nmf_single_type = function(..., k_list, reference_cat, stage, cohort,
+                           filter_dn, min_exposure, keep_sigs) {
+  TIME = as.POSIXct(Sys.time(), format = "%H:%M:%S")
+  call_info = match.call()
+
+  if (!is.null(reference_cat)) {
+    x_ref = pyfit(..., k_list=0, reference_cat=reference_cat,
+                  stage="random_noise", filter_dn=FALSE)
+
+    x_ref_filt = x_ref %>% filter_sigs_low_expos(min_exp=min_exposure, keep_sigs=keep_sigs)
+    catalogue2 = long_to_wide(dataframe=x_ref_filt$beta_fixed, what="beta")
+  } else {
+    x_ref = x_ref_filt = catalogue2 = NULL
+    residues = FALSE
+  }
+
+  x_dn = pyfit(..., k_list=k_list, reference_cat=catalogue2, stage=stage, filter_dn=filter_dn)
+
+  TIME = difftime(as.POSIXct(Sys.time(), format = "%H:%M:%S"), TIME, units = "mins")
+
+  x_dn$time = TIME
+  x_dn$k_list = k_list
+  x_dn$call = call_info
+
+  return(x_dn)
+}
+
+
+renormalize_denovo_thr = function(beta_denovo, thr=0.02, filter_dn=TRUE) {
+  if (is.null(beta_denovo)) return(NULL)
+
+  return(
+    beta_denovo %>%
+      dplyr::mutate(value=replace(value, value<thr, 0)) %>%
+      dplyr::group_by(sigs) %>%
+      dplyr::mutate(value=value/sum(value)) %>%
+      dplyr::ungroup()
+  )
+}
+
+
+
+
