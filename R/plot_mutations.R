2e59c67983fb222ac8fc0f1849e3c6bd0d0ae909
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-12-14 17:54:56 +0100

    mc

--- R/plot_mutations.R
+++ R/plot_mutations.R
@@ -4,16 +4,20 @@ plot_data = function(x, types=get_types(x), samples=get_samples(x),
   counts_list = get_input(x, types=types, samples=samples, clusters=clusters,
                           reconstructed=reconstructed)
 
-  counts = lapply(types, function(tid) {
+  patch = lapply(types, function(tid) {
     counts_list[[tid]] %>% reformat_contexts(what=tid) %>%
-      dplyr::mutate(type=!!tid)
-    }) %>% do.call(rbind, .) %>%
-    dplyr::filter(samples %in% !!samples)
+      dplyr::mutate(type=!!tid) %>%
+      dplyr::filter(samples %in% !!samples) %>%
+      ggplot() +
+      geom_histogram(aes(x=context, y=value), stat="identity") +
+      facet_grid(type ~ variant, scales="free", space="free_x") +
+      theme_bw()
+    }) %>%
+    patchwork::wrap_plots(nrow=length(types)) & ylab(NULL) & xlab(NULL) & theme(plot.margin = margin(5.5, 5.5, 5.5, 0))
 
-  counts %>%
-    ggplot() +
-    geom_histogram(aes(x=context, y=value), stat="identity") +
-    ggh4x::facet_nested(.~type+variant, scales="free") +
-    theme_bw()
+  patchwork::wrap_elements(patch) +
+    labs(tag="Context") +
+    theme(plot.tag=element_text(size=rel(1), angle=0),
+          plot.tag.position="bottom")
 
 }
