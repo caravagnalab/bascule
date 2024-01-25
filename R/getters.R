7426e5c7902bc6ddc4b13f075fafaf25cef31803
Elena Buscaroli <ele.buscaroli@gmail.com>
2024-01-25 17:22:06 +0100

    Update getters.R
    reorganization and adding two getters

--- R/getters.R
+++ R/getters.R
@@ -1,21 +1,10 @@
-get_K = function(x, types=get_types(x)) {
-  return(lapply(get_signames(x, types=types), length))
+# Signatures #####
+## names ####
+get_signames = function(x, types=get_types(x)) {
+  lapply(types, function(t)
+    get_signatures(x, types=t, matrix=TRUE)[[t]] %>% rownames()) %>%
+    setNames(types)
 }
-
-
-get_G = function(x, input=FALSE) {
-  if (!input) return(length(get_cluster_labels(x)))
-  return(
-    get_pyro_stat(x, what="clustering",
-                  statname="params")[["infered_params"]]$post_probs %>% ncol()
-  )
-}
-
-
-get_seed = function(x, what, types=get_types(x)) {
-  return(get_pyro_stat(x, what=what, statname="seed"))
-}
-
 
 get_fixed_signames = function(x, types=get_types(x)) {
   lapply(types, function(t)
@@ -29,12 +18,10 @@ get_denovo_signames = function(x, types=get_types(x)) {
     setNames(types)
 }
 
-get_signames = function(x, types=get_types(x)) {
-  lapply(types, function(t)
-    get_signatures(x, types=t, matrix=TRUE)[[t]] %>% rownames()) %>%
-    setNames(types)
+## signature profiles #####
+get_signatures = function(x, types=get_types(x), matrix=FALSE) {
+  return(get_signatures_aux(x=x, types=types, matrix=matrix, what="all"))
 }
-
 
 get_fixed_signatures = function(x, types=get_types(x), samples=get_samples(x),
                                 clusters=get_cluster_labels(x), matrix=FALSE) {
@@ -47,15 +34,8 @@ get_denovo_signatures = function(x, types=get_types(x), samples=get_samples(x),
   return(get_signatures_aux(x=x, types=types, matrix=matrix, what="denovo"))
 }
 
-
-# Get signatures, it can subset by types and return a list of matrices.
-get_signatures = function(x, types=get_types(x), matrix=FALSE) {
-  return(get_signatures_aux(x=x, types=types, matrix=matrix, what="all"))
-}
-
-
-## what %in% c("denovo","fixed","all")
 get_signatures_aux = function(x, what, types=get_types(x), matrix=FALSE) {
+  ## what %in% c("denovo","fixed","all")
   out = lapply(types, function(t) {
     if (what=="fixed") x$nmf[[t]]$beta_fixed else if (what=="denovo")
       x$nmf[[t]]$beta_denovo else if (what=="all")
@@ -69,6 +49,8 @@ get_signatures_aux = function(x, what, types=get_types(x), matrix=FALSE) {
 }
 
 
+
+# Input #####
 get_input = function(x, types=get_types(x), samples=get_samples(x),
                      clusters=get_cluster_labels(x), matrix=FALSE,
                      reconstructed=FALSE, add_groups=FALSE) {
@@ -87,8 +69,7 @@ get_input = function(x, types=get_types(x), samples=get_samples(x),
     if(!is.null(clusters)) {
       clusters_df = x %>%
         get_cluster_assignments(samples=samples, clusters=clusters)
-
-      w = w %>% dplyr::right_join(clusters_df)
+      w = w %>% dplyr::right_join(clusters_df, by="samples")
     }
 
     return(w)
@@ -101,6 +82,21 @@ get_input = function(x, types=get_types(x), samples=get_samples(x),
   return(out)
 }
 
+get_input_signatures = function(x, types=get_types(x), matrix=F) {
+  lapply(types, function(tid) {
+    sigs = x$input[[tid]]$reference
+    if (matrix) sigs = long_to_wide(sigs, what="beta")
+  }) %>% setNames(types)
+}
+
+get_input_signames = function(x, types=get_types(x)) {
+  sigs = get_input_signatures(x, types=types, matrix=T)
+  lapply(sigs, rownames) %>% setNames(names(sigs))
+}
+
+
+
+# Exposures ####
 # Get exposure, it can subset by types, samples and clusters. It can return
 # a list of matrices.
 get_exposure = function(x, types=get_types(x), samples=get_samples(x),
@@ -127,17 +123,25 @@ get_exposure = function(x, types=get_types(x), samples=get_samples(x),
 }
 
 
-get_beta_weights = function(x, types=get_types(x)) {
-  if (is.null(get_params(x, what="nmf", types=types[1])[[1]]$beta_w))
-    return(NULL)
-  lapply(types, function(tid) {
-    get_params(x, what="nmf", types=tid)[[1]]$beta_w %>%
-      tibble::rownames_to_column(var="sigid") %>%
-      reshape2::melt(variable.name="sigs") %>%
-      dplyr::mutate(type=tid)
-  }) %>% do.call(rbind, .)
-}
 
+# Clustering #####
+get_centroids = function(x, matrix=F) {
+  if (is.null(x$clustering$centroids)) return(NULL)
+
+  unq_labels = get_cluster_labels(x)
+
+  centr = x$clustering$centroids %>%
+    dplyr::mutate(clusters=paste0("G",stringr::str_replace_all(clusters,"G","")),
+                  sigs=stringr::str_replace_all(sigs,"^[0-9]+_","")) %>%
+    dplyr::filter(clusters %in% unq_labels)
+
+  if (matrix)
+    return(centr %>%
+             tidyr::pivot_wider(names_from="sigs", values_from="value") %>%
+             tibble::column_to_rownames(var="clusters"))
+
+  return(centr)
+}
 
 get_mixing_proportions = function(x) {
   if (!have_groups(x)) return(NULL)
@@ -146,8 +150,6 @@ get_mixing_proportions = function(x) {
   cnames = paste0("G",1:length(pis)-1)
   data.frame(value=pis, clusters=factor(cnames, levels=cnames))
 }
-
-
 
 # Get cluster labels ("C1", "C2")
 get_cluster_labels = function(x) {
@@ -160,13 +162,13 @@ get_cluster_labels = function(x) {
 get_cluster_assignments = function(x, samples=get_samples(x), clusters=get_cluster_labels(x)) {
   if(is.null(x$clustering)) return(NULL)
 
-  return(
-    x$clustering$clusters %>%
-      dplyr::filter(samples %in% !!samples,
-                    clusters %in% !!clusters)
-  )
+  x$clustering$clusters %>%
+    dplyr::filter(samples %in% !!samples, clusters %in% !!clusters)
 }
 
+
+
+# Fit infos #####
 
 # Get type of data used for signatures: e.g., "SBS", "DBS"
 get_types = function(x) {
@@ -174,16 +176,33 @@ get_types = function(x) {
   return(x$input %>% names())
 }
 
-
 get_fittypes = function(x) {
   if (is.null(x)) return(NULL)
   return(names(x)[names(x)!="input"])
 }
 
-
 # Get samples names
 get_samples = function(x) {x$input[[1]]$counts$samples %>% unique()}
 
+get_K = function(x, types=get_types(x)) {
+  return(lapply(get_signames(x, types=types), length))
+}
+
+get_G = function(x, input=FALSE) {
+  if (!input) return(length(get_cluster_labels(x)))
+  return(
+    get_pyro_stat(x, what="clustering",
+                  statname="params")[["infered_params"]]$post_probs %>% ncol()
+  )
+}
+
+get_seed = function(x, what, types=get_types(x)) {
+  return(get_pyro_stat(x, what=what, statname="seed"))
+}
+
+
+
+# Auxiliary fns #####
 
 # what %in% c("beta","exposures","counts")
 wide_to_long = function(dataframe, what) {
@@ -216,23 +235,15 @@ long_to_wide = function(dataframe, what) {
 }
 
 
-get_centroids = function(x, matrix=F) {
-  if (is.null(x$clustering$centroids)) return(NULL)
 
-  unq_labels = get_cluster_labels(x)
-
-  centr = x$clustering$centroids %>%
-    dplyr::mutate(clusters=paste0("G",stringr::str_replace_all(clusters,"G","")),
-                  sigs=stringr::str_replace_all(sigs,"^[0-9]+_","")) %>%
-    dplyr::filter(clusters %in% unq_labels)
-
-  if (matrix)
-    return(centr %>%
-             tidyr::pivot_wider(names_from="sigs", values_from="value") %>%
-             tibble::column_to_rownames(var="clusters"))
-
-  return(centr)
+# Deprecated ####
+get_beta_weights = function(x, types=get_types(x)) {
+  if (is.null(get_params(x, what="nmf", types=types[1])[[1]]$beta_w))
+    return(NULL)
+  lapply(types, function(tid) {
+    get_params(x, what="nmf", types=tid)[[1]]$beta_w %>%
+      tibble::rownames_to_column(var="sigid") %>%
+      reshape2::melt(variable.name="sigs") %>%
+      dplyr::mutate(type=tid)
+  }) %>% do.call(rbind, .)
 }
-
-
-
