5c5ed9b7db3cdc409490a5c95d443daa7838873e
azadsadr <azad.sadr.h@gmail.com>
2024-02-13 11:53:19 +0100

    removed hard-coded part and generalized the refinement function

--- R/utils_refine.R
+++ R/utils_refine.R
@@ -4,11 +4,13 @@
 # comparing similarity between all possible pairs of denovo-fixed and denovo-denovo signatures
 # if there is similarity higher than threshold then remove the denovo signature
 # and adding its exposure contribution to the corresponding pair signature
-refine_signatures_SBS <- function(x, threshold=0.8) {
+refine_signatures_aux <- function(x, type, threshold) {
   
-  exposure <- basilica:::long_to_wide(x$nmf$SBS$exposure, what = "exposures")
-  fixed <- basilica:::long_to_wide(x$nmf$SBS$beta_fixed, what = "beta")
-  denovo <- basilica:::long_to_wide(x$nmf$SBS$beta_denovo, what = "beta")
+  #types <- basilica:::get_types(x)
+  exposure <- basilica:::get_exposure(x, matrix = TRUE)[[type]]
+  fixed <- basilica:::get_fixed_signatures(x, matrix = TRUE)[[type]]
+  denovo <- basilica:::get_denovo_signatures(x, matrix = TRUE)[[type]]
+  
   a <- basilica:::cosine.matrix(fixed, denovo)
   while (max(a) >= threshold) {
     row <- which(a == max(a), arr.ind = T)[1]
@@ -39,53 +41,20 @@ refine_signatures_SBS <- function(x, threshold=0.8) {
     b[sig2, ] <- 0 # make zero row includes the removed denovo signature in cosine matrix
   }
   
-  x$nmf$SBS_refined$exposure <- basilica:::wide_to_long(exposure, what = "exposures")
-  x$nmf$SBS_refined$beta_fixed <- x$nmf$SBS$beta_fixed
-  x$nmf$SBS_refined$beta_denovo <- basilica:::wide_to_long(denovo, what = "beta")
-  
-  return(x)
+  return(
+    list(
+      exposure=basilica:::wide_to_long(exposure, what = "exposures"), 
+      fixed=basilica:::wide_to_long(fixed, what = "beta"), 
+      denovo=basilica:::wide_to_long(denovo, what = "beta")
+    )
+  )
 }
 
 
-refine_signatures_DBS <- function(x, threshold=0.8) {
-  
-  exposure <- basilica:::long_to_wide(x$nmf$DBS$exposure, what = "exposures")
-  fixed <- basilica:::long_to_wide(x$nmf$DBS$beta_fixed, what = "beta")
-  denovo <- basilica:::long_to_wide(x$nmf$DBS$beta_denovo, what = "beta")
-  a <- basilica:::cosine.matrix(fixed, denovo)
-  while (max(a) >= threshold) {
-    row <- which(a == max(a), arr.ind = T)[1]
-    col <- which(a == max(a), arr.ind = T)[2]
-    sigF <- rownames(a[row, ])
-    sigD <- colnames(a[col])
-    exposure[, sigF] <- exposure[, sigF] + exposure[, sigD]
-    exposure[, sigD] <- NULL # remove the denovo signature from exposure matrix
-    denovo <- denovo[!(rownames(denovo) %in% sigD), ] # remove the denovo signature from denovo matrix
-    a[, col] <- 0 # make zero the denovo signature column in cosine matrix
-  }
-  
-  b <- basilica:::cosine.matrix(denovo, denovo)
-  for (i in 1:nrow(b)) {
-    for (j in 1:i) {
-      b[i,j] <- 0
-    }
-  }
-  while (max(b) >= threshold) {
-    row <- which(b == max(b), arr.ind = T)[1]
-    col <- which(b == max(b), arr.ind = T)[2]
-    sig1 <- rownames(b[row, ])
-    sig2 <- colnames(b[col])
-    exposure[, sig1] <- exposure[, sig1] + exposure[, sig2]
-    exposure[, sig2] <- NULL # remove the denovo signature from exposure matrix
-    denovo <- denovo[!(rownames(denovo) %in% sig2), ] # remove the denovo signature from denovo matrix
-    b[, sig2] <- 0 # make zero column includes the removed denovo signature in cosine matrix
-    b[sig2, ] <- 0 # make zero row includes the removed denovo signature in cosine matrix
-  }
-  
-  x$nmf$DBS_refined$exposure <- basilica:::wide_to_long(exposure, what = "exposures")
-  x$nmf$DBS_refined$beta_fixed <- x$nmf$DBS$beta_fixed
-  x$nmf$DBS_refined$beta_denovo <- basilica:::wide_to_long(denovo, what = "beta")
-  
+refine_signatures <- function(x, threshold=0.8) {
+  xxx <- lapply(basilica:::get_types(x), refine_signatures_aux, x=x, threshold=threshold)
+  names(xxx) <- paste(basilica:::get_types(x), "refined", sep = "_")
+  x$nmf$refined <- xx
   return(x)
 }
 
