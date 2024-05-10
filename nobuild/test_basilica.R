b2c1eb99dc9e0470ade2280d5eb0ab9ee6bb1b72
Elena Buscaroli <ele.buscaroli@gmail.com>
2024-05-10 14:50:52 +0200

    Update test_basilica.R

--- nobuild/test_basilica.R
+++ nobuild/test_basilica.R
@@ -6,10 +6,14 @@ counts = get_input(example_dataset, matrix=T)
 reference_cat = list("SBS"=COSMIC_sbs_filt, "DBS"=COSMIC_dbs)
 max_K = sapply(get_signames(example_dataset), length) %>% max()
 K_list = 0:2
-x = fit(counts=counts, k_list=K_list, n_steps=1000,
+x = fit(counts=counts, k_list=K_list, n_steps=3000,
         reference_cat=reference_cat, keep_sigs=c("SBS1","SBS5"), store_fits=TRUE)
 x_refined = refine_denovo_signatures(x)
 x_refined_cluster = fit_clustering(x_refined, cluster=3)
 
+example_dataset %>% plot_exposures()
+x_refined_cluster %>% merge_clusters() %>% plot_exposures()
 
+example_dataset %>% plot_signatures()
+x_refined_cluster %>% merge_clusters() %>% plot_signatures()
 
