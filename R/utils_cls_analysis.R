abe10dc325ef7d0f460dc511c0e6912cd0566a95
Azad Sadr <azad.sadr@proton.me>
2024-05-23 17:26:13 +0200

    add plotting function for signatures score in clusters

--- R/utils_cls_analysis.R
+++ R/utils_cls_analysis.R
@@ -132,3 +132,127 @@ get_clusters_score = function(x, types=get_types(x), exposure_thr=0.05, quantile
 #}
 
 
+# ==============================================================================
+# ==============================================================================
+# VISUALIZAION
+# ==============================================================================
+# ==============================================================================
+
+# input : basilica object
+# output: ggplot (samples frequency in clusters)
+plot_cluster_freq <- function(x) {
+  df <- basilica:::get_cluster_assignments(x) # dataframe
+  p <- ggplot(df, aes(x = clusters)) + 
+    geom_bar(stat = "count", fill = "purple") +
+    labs(x = "Clusters", y = "Number of samples", title = "Distribution of samples in clusters") + 
+    theme_minimal()
+  return(p)
+}
+
+#-------------------------------------------------------------------------------
+
+# plot the final score for a cluster and single type
+# a : scores data-frame
+# b : single context type
+# c : cluster name
+plot_scores_aux1_1 <- function(a, b, c) {
+  
+  # filter on context type and cluster name
+  df <- a %>% subset(type == b & cluster == c)
+  
+  # find the quantile value
+  #val <- df %>% dplyr::pull(score) %>% quantile(probs = c(0.9))
+  #df1 <-df[c(1,2,6)]
+  
+  p <- ggplot(df, aes(x = signature, y = score, color = cluster, group = cluster)) + 
+    geom_line() +
+    geom_point() +
+    labs(x = "Mutational Signature", y = "Value", title = paste0("Signatures Score (Final) in Cluster: ", b)) + 
+    geom_hline(yintercept = df$score_quantile, linetype = "dashed", color = "black") + 
+    theme_minimal()
+  
+  return(p)
+}
+
+#-------------------------------------------------------------------------------
+
+# plot the final score for a cluster and all context types
+# a : scores data-frame
+# b : all context types
+# c : cluster name
+plot_scores_aux1 <- function(a, b, c) {
+  
+  plot_list <- lapply(b, function(tid) plot_scores_aux1_1(a, tid, c))
+  
+  p <- patchwork::wrap_plots(
+    plot_list, 
+    nrow = length(plot_list), 
+    ncol = 1, 
+    guides = "collect", 
+    tag_level = "keep", 
+    design = "AAAA
+              BBBB",
+    axes = "collect"
+  )
+  
+  return(p)
+}
+
+#-------------------------------------------------------------------------------
+
+# plot the scores ingredients for single cluster
+# a : scores data-frame
+# b : single context type
+# c : cluster name
+plot_scores_aux2_1 <- function(a, b, c) {
+  
+  df <- a %>% subset(type == b & cluster == c)
+  
+  df_long <- tidyr::pivot_longer(df[c(1,2,3,4,5)], cols = c(varRatio, activeRatio, mutRatio), names_to = "name", values_to = "value")
+  p <- ggplot(subset(df_long, cluster == c), aes(x = signature, y = value, color = name, group = name)) + 
+    geom_line() + 
+    geom_point() + 
+    labs(x = "Mutational Signature", y = "Value", title = paste0("Signatures Score (Ingredients) in Cluster: ", b)) + 
+    theme_minimal()
+  return(p)
+}
+
+#-------------------------------------------------------------------------------
+
+plot_scores_aux2 <- function(a, b, c) {
+  
+  plot_list <- lapply(b, function(tid) plot_scores_aux2_1(a, tid, c))
+  
+  p <- patchwork::wrap_plots(
+    plot_list, 
+    nrow = length(plot_list), 
+    ncol = 1, 
+    guides = "collect", 
+    tag_level = "keep", 
+    design = "AAAA
+              BBBB",
+    axes = "collect"
+  )
+  
+  return(p)
+}
+
+#-------------------------------------------------------------------------------
+
+plot_cluster_scores <- function(x, types=get_types(x), clusterName, exposure_thr = 0.05, quantile_thr = 0.9) {
+  
+  a <- basilica:::get_clusters_score(x, types, exposure_thr, quantile_thr)
+  
+  p1 <- plot_scores_aux1(a, types, clusterName)
+  p2 <- plot_scores_aux2(a, types, clusterName)
+  
+  p3 <- basilica:::plot_exposures(x, types, clusters = clusterName, elim_thr = exposure_thr)
+  
+  #p3 <- basilica::plot_exposures(x = x, types = stypes, clusters = clusterName)
+  
+  #p3 <- plot.cluster.exposure(x, types, clusterName, threshold)
+  
+  return( (p1 | p2) / p3 )
+}
+
+
