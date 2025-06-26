8ad81608ad6cb586e90e31530f2a13b6953d302f
Elena Buscaroli <ele.buscaroli@gmail.com>
2025-06-26 12:58:01 +0800

    fixing minor bug in plotting

--- R/utils_plot.R
+++ R/utils_plot.R
@@ -1,29 +1,33 @@
 reformat_contexts = function(signatures, what) {
-  if(what == "SBS") {
+  if (grepl("SBS", what)) {
     signatures = signatures %>%
       dplyr::mutate(variant=paste0(substr(start=3, stop=3, features), ">", substr(start=5, stop=5, features)),
                     context=paste0(substr(start=1, stop=1, features), '_', substr(start=7, stop=7, features)))
   }
 
-  if(what == "DBS") {
+  else if (grepl("DBS", what)) {
     signatures = signatures %>%
       dplyr::mutate(variant=paste0(substr(start=1, stop=2, features),">NN"),
                     context=substr(start=4, stop=5, features))
   }
 
-  if(what == "ID") {
+  else if (grepl("ID", what)) {
     signatures = signatures %>%
       dplyr::mutate(features=as.character(features),
                     variant=substr(start=1, stop=nchar(features) - 2, features),
                     context=substr(start=nchar(features), stop=nchar(features), features))
   }
 
-  if(what == "CNV") {
+  else if (grepl("CN", what)) {
     signatures = signatures %>%
       dplyr::mutate(features=as.character(features),
                     variant=paste0(str_split(features, pattern=":")[[1]][1], ":",
                                    str_split(features, pattern=":")[[1]][2]),
                     context= paste0(str_split(features,pattern=":")[[1]][3]))
+  }
+
+  else {
+    cli::cli_alert_warning("Not able to reformat the contexts to plot signatures.")
   }
 
   return(signatures)
